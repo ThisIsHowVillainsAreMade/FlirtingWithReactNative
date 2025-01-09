d2b6ba54f32260e4072759b5914cf52c9d70740c
ThisIsHowVillainsAreMade <okalfa@outlook.fr>
2025-01-09 15:51:53 +0100

    on test

--- App.js
+++ App.js
@@ -1,27 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from "expo-status-bar";
+// import { Button, StyleSheet, Text, View } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.hello}>Bienvenue sur IPhone !</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text style={styles.hello}>Jouons avec React Native !</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor:'black'
-  },
-  hello: {
-    color:'ivory',
-    fontWeight:'bold',
-    fontSize:70
-  }
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//     backgroundColor: "black",
+//   },
+//   hello: {
+//     color: "ivory",
+//     fontWeight: "bold",
+//     fontSize: 70,
+//   },
+// });
+import React from 'react';
+import { View, Text } from 'react-native';
 
-});
+const App = () => (
+  <View style={{ flex:1, backgroundColor:'white'}}>
+  <Text style={{ flex: 1, justifyContent: 'center'}}>
+    The quick brown fox jumps over the lazy dog
+  </Text></View>
+);
+
+export default App;
