523e6ee9f9086b7b626e74daf37f62b236464615
ThisIsHowVillainsAreMade <okalfa@outlook.fr>
2025-01-09 15:33:52 +0100

    on fait des carrés rouge et bleu

--- App.js
+++ App.js
@@ -1,27 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+// import { Button, StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.hello}>Bienvenue sur IPhone !</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text style={styles.hello}>Hello DuckSociety !</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor:'black'
-  },
-  hello: {
-    color:'ivory',
-    fontWeight:'bold',
-    fontSize:70
-  }
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//     backgroundColor:'black'
+//   },
+//   hello: {
+//     color:'ivory',
+//     fontWeight:'bold',
+//     fontSize:50
+//   }
 
-});
+// });
+import React from 'react';
+import { View } from 'react-native';
+
+const App = () => (
+  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View style={{ width: 100, height: 100, backgroundColor: 'red' }} />
+    <View style={{ width: 100, height: 100, backgroundColor: 'blue' }} />
+  </View>
+);
+
+export default App;
