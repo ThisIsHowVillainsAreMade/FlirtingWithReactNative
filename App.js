078368ae30474c6f90c6dbdaa077350af0bbcbe9
ThisIsHowVillainsAreMade <okalfa@outlook.fr>
2025-01-09 15:18:18 +0100

    discover of react native

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.hello}>Bienvenue sur IPhone !</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +16,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor:'black'
   },
+  hello: {
+    color:'ivory',
+    fontWeight:'bold',
+    fontSize:70
+  }
+
 });
