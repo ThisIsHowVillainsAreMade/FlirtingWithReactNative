f11e78eb064ad336dd38fb202d2444a1249bbc70
ThisIsHowVillainsAreMade <okalfa@outlook.fr>
2025-01-09 15:57:33 +0100

    play with text

--- App.js
+++ App.js
@@ -1,36 +1,10 @@
-// import { StatusBar } from "expo-status-bar";
-// import { Button, StyleSheet, Text, View } from "react-native";
-
-// export default function App() {
-//   return (
-//     <View style={styles.container}>
-//       <Text style={styles.hello}>Jouons avec React Native !</Text>
-//       <StatusBar style="auto" />
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: "#fff",
-//     alignItems: "center",
-//     justifyContent: "center",
-//     backgroundColor: "black",
-//   },
-//   hello: {
-//     color: "ivory",
-//     fontWeight: "bold",
-//     fontSize: 70,
-//   },
-// });
 import React from 'react';
 import { View, Text } from 'react-native';
 
 const App = () => (
-  <View style={{ flex:1, backgroundColor:'white'}}>
-  <Text style={{ flex: 1, justifyContent: 'center'}}>
-    The quick brown fox jumps over the lazy dog
+  <View style={{ flex:1, height:100}}>
+  <Text style={{ flex: 1, justifyContent: 'center', fontSize:16, color:'white', top:50}}>
+    The <Text style={{fontWeight:'bold'}}>quick brown fox</Text> jumps over the lazy dog
   </Text></View>
 );
 
