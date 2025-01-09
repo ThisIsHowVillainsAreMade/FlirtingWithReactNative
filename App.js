4575e97bcf0fb402a9d316d9e5ace85becc0317a
ThisIsHowVillainsAreMade <okalfa@outlook.fr>
2025-01-09 16:17:51 +0100

    play with img and testing

--- App.js
+++ App.js
@@ -1,27 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Image, View } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.hello}>Jouons avec ReactNative !</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const img = require('./assets/favicon.png');
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor:'black'
-  },
-  hello: {
-    color:'ivory',
-    fontWeight:'bold',
-    fontSize:70
-  }
+const App = () => (
+  <View style={{ flex: 1, justifyContent: "center" }}>
+    <Image
+      style={{ width: 100, height: 100 }}
+      source={{
+        uri: "https://www.powertrafic.fr/wp-content/uploads/2023/04/image-ia-exemple.png",
+      }}
+    ></Image>
+    <Image
+      style={{ width: 100, height: 100 }}
+      source={img}
+    ></Image>
+  </View>
+);
 
-});
+export default App;
