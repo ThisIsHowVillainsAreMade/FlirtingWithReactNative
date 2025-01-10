c5f701f1f7370606cf611d0f0503a2874c222267
ThisIsHowVillainsAreMade <okalfa@outlook.fr>
2025-01-10 12:17:33 +0100

    scrollView

--- App.js
+++ App.js
@@ -1,27 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { ScrollView, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.hello}>Jouons avec ReactNative !</Text>
-      <StatusBar style="auto" />
+const App = () => (
+  <View style={{ flex: 1, justifyContent: 'center' }}>
+    <Text style={{ fontSize: 24, textAlign: 'center' }}>
+      Scroll me!
+    </Text>
+    <View style={{ height: 400, backgroundColor: '#e5e5e5' }}>
+      {/* This is our scrollable area */}
+      <ScrollView horizontal>
+        <View style={{ width: 300, height: 300, backgroundColor: 'red' }} />
+        <View style={{ width: 300, height: 300, backgroundColor: 'green' }} />
+        <View style={{ width: 300, height: 300, backgroundColor: 'blue' }} />
+      </ScrollView>
     </View>
-  );
-}
+  </View>
+);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor:'black'
-  },
-  hello: {
-    color:'ivory',
-    fontWeight:'bold',
-    fontSize:70
-  }
-
-});
+export default App;
