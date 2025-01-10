be8368d222291b5633bd0132b608c462ae94e202
ThisIsHowVillainsAreMade <okalfa@outlook.fr>
2025-01-10 14:29:29 +0100

    button

--- App.js
+++ App.js
@@ -1,27 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, Text, View } from 'react-native';
 
-export default function App() {
+const App = () => {
+  const [pressedCount, setPressedCount] = useState(0);
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.hello}>Jouons avec ReactNative !</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: 'center' }}>
+      <Text style={{ margin: 16 }}>
+        {pressedCount > 0
+          ? `The button was pressed ${pressedCount} times!`
+          : 'The button isn\'t pressed yet'
+        }
+      </Text>
+      <Button
+        title='Press me'
+        onPress={() => setPressedCount(pressedCount + 1)}
+        disabled={pressedCount >= 3}
+      />
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor:'black'
-  },
-  hello: {
-    color:'ivory',
-    fontWeight:'bold',
-    fontSize:70
-  }
-
-});
+export default App;
